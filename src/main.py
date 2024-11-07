a5184ad72283309906aadfb5dfc657646a081354
Gabe <gabrielgrechuk@outlook.com>
2024-11-07 13:42:31 -0300

    #4 - Basic rendering method

--- src/main.py
+++ src/main.py
@@ -2,7 +2,18 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem import AllChem
 
-m = Chem.MolFromSmiles('NC(O)C(=O)O')
 
-img = Draw.MolToImage(m)
-img.save('molecule.png')
+def render_smile(smile: str, output_path: str):
+    print(f"Rendering {smile} to {output_path}")
+    m = Chem.MolFromSmiles(smile)
+    img = Draw.MolToImage(m)
+    img.save(output_path)
+    print("Done!")
+
+
+def main():
+    render_smile("NC(O)C(=O)O", "./molecule.png")
+
+
+if __name__ == "__main__":
+    main()
